f0759fb6f3b8e91b5f762c9154ecf5f33161a7c7
Richard Peck <rpeck@frontlineutilities.co.uk>
2020-04-03 12:23:42 +0100

    Readme

--- config/constants.rb
+++ config/constants.rb
@@ -15,13 +15,7 @@
 DOMAIN              = ENV.fetch('DOMAIN', 'fox-and-oak.myshopify.com') ## used for CORS and other funtionality -- ENV var gives flexibility
 DEBUG               = ENV.fetch("DEBUG", false) != false ## this needs to be evaluated this way because each ENV variable returns a string ##
 ENVIRONMENT         = ENV.fetch("RACK_ENV", "development")
-SHARED_SECRET       = ENV.fetch("SECRET", false)
-SENDGRID_API_KEY    = ENV.fetch("SENDGRID_API_KEY", nil)
-SENDGRID_USERNAME   = ENV.fetch("SENDGRID_USERNAME", nil)
-PWINTY_SKU          = {
-                        framed: { A4: "FRA-CLA-HGE-MOUNT1-GLA-A4", A3: "FRA-CLA-HGE-MOUNT1-GLA-A3" },
-                        normal: { A4: "GLOBAL-HGE-A4", A3: "GLOBAL-HGE-A3" }
-                      }
+HMRC_API_ENDPOINT   = ENV.fetch("HMRC_API_ENDPOINT", "https://test-api.service.hmrc.gov.uk") # => production or sandbox URL
 
 ##########################################################
 ##########################################################
